aa751081a98befccdd1abe5fc0aa7806369b610b
Dan Bjorge <dan@dbjorge.net>
2018-02-28 01:42:53 -0800

    Implement better animal search criteria defaults

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# Requirements (priority order)
+1. [done] Prevent session timeout
+2. [done] Use better default animal search options (by name, active only)
+3. Make giant "search for another dog", "see DBS notes", and "add DBS note" buttons
+4. Add selector panels/name/etc fields to details page
+5. Make "see DBS notes" render inline instead of in a popup
+6. Suppress unnecessary PetPoint elements (main nav menu especially)
--- script/injected_script.js
+++ script/injected_script.js
@@ -9,4 +9,56 @@ function preventSessionTimeout() {
     }
 }
 
+function setAnimalSearchCriteriaDefaults() {
+    var searchCriteriaSelectElement = $('table.search select[id$=_ctrlAnimalSearch_ddlCriteria]');
+
+    if (searchCriteriaSelectElement.length === 0) {
+        console.debug('No search UI on page, skipping setAnimalSearchCriteriaDefaults');
+        return;
+    }
+
+    var nameOption = searchCriteriaSelectElement.children().filter(function() { return $(this).text() == "Name" } );
+    var nameOptionValue = nameOption.attr('value');
+
+    console.debug('Setting criteria to Name, detected as value ' + nameOptionValue);
+    searchCriteriaSelectElement.val(nameOptionValue).change();
+
+    var dynamicSetAttempts = 0;
+    var dynamicSetRetryDelayMs = 100;
+    var dynamicSetMaxAttempts = 50;
+    var setDefaultsForDynamicallyAddedElements = function() {
+        dynamicSetAttempts++;
+        console.debug('entering setDefaultsForDynamicallyAddedElements attempt ' + dynamicSetAttempts);
+
+        // It's important not to cache the table.search element from the non-dynamic part because
+        // the whole table.search element gets replaced during the search criteria UI update
+        var activeOnlyRadioInput = $('table.search input[id$=_ctrlAnimalSearch_CB_OnlyActive_1]');
+        var animalNameInputBox = $('table.search input[id$=_ctrlAnimalSearch_txtFirstCriteria]');
+
+        if (activeOnlyRadioInput.length === 0 || animalNameInputBox.length === 0) {
+            if (dynamicSetAttempts > dynamicSetMaxAttempts) {
+                console.error('Giving up on finding dynamically added search elements');
+            } else {
+                console.debug('Didn\'t find dynamically added search elements, scheduling another try...');
+                // This is important for the case where our original change to the element happened before PetPoint's
+                // onchange handler was dynamically registered
+                searchCriteriaSelectElement.change();
+                setTimeout(setDefaultsForDynamicallyAddedElements, dynamicSetRetryDelayMs);
+            }
+            return;
+        }
+
+        console.debug('Selecting Active Only radio input');
+        activeOnlyRadioInput.prop("checked", true).change();
+
+        console.debug('Assigning focus to the Animal Name input box');
+        animalNameInputBox.focus();
+    };
+    setTimeout(setDefaultsForDynamicallyAddedElements, 1000);
+}
+
 preventSessionTimeout();
+
+// The 1s delay is required for the New Care Activity page, but not the Edit Animal page
+// Would be nice to find a more deterministic way to deal with New Care Activity
+setTimeout(setAnimalSearchCriteriaDefaults, 1000);
